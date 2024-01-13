f44bbe5b7cdb50e3e90aafe2b23975725992610a
Johnebhin <121502677+Johnebhin@users.noreply.github.com>
2024-01-13 10:41:23 +0530

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,8 @@
+import os,shutil
+
+
+from_dir="C:/Users/Subramani/Downloads/downloaded"
+to_dir="C:/Users/Subramani/Pictures/Screenshots"
+
+list_of_files=os.listdir(from_dir)
+print(list_of_files)
